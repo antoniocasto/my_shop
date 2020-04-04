95cf8110722103ec049f67dc85dbf4d690819b97
antoniocasto <antonio9_4@outlook.com>
2020-04-04 17:29:57 +0200

    Chapter 9 - end of lesson 3 - Showing Alert Dialogs

--- my_shop/lib/widgets/cart_item_tile.dart
+++ my_shop/lib/widgets/cart_item_tile.dart
@@ -22,6 +22,37 @@ class CartItemTile extends StatelessWidget {
     return Dismissible(
       key: ValueKey(id),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text(
+              'Are you sure?',
+            ),
+            content: Text(
+              'Do you want to remove the item from the cart?',
+            ),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  //Il context dentro il dialog in e' quello del suo builder
+                  //confirmDismiss si aspetta un Future booleano che possiamo
+                  //tornare con il pop. Mettere un return dopo il pop non ha senso perche'
+                  //non viene chiamato
+                  Navigator.of(ctx).pop(false);
+                },
+                child: Text('No'),
+              ),
+              FlatButton(
+                onPressed: () {
+                  Navigator.of(ctx).pop(true);
+                },
+                child: Text('Yes'),
+              ),
+            ],
+          ),
+        );
+      },
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
