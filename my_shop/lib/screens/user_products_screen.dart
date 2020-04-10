de8bdb38f026aa9d90cb95ee25be1e2a8c277261
antoniocasto <antonio9_4@outlook.com>
2020-04-10 13:10:52 +0200

    Chapter 9 - end of lesson 6- Using Forms & Working with Form Inputs

--- my_shop/lib/screens/user_products_screen.dart
+++ my_shop/lib/screens/user_products_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_shop/providers/products.dart';
+import 'package:my_shop/screens/edit_product_screen.dart';
 import 'package:my_shop/widgets/app_drawer.dart';
 import 'package:my_shop/widgets/user_product_item.dart';
 import 'package:provider/provider.dart';
@@ -18,7 +19,7 @@ class UserProductsScreen extends StatelessWidget {
           IconButton(
             icon: const Icon(Icons.add),
             onPressed: () {
-              //...
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
             },
           ),
         ],
